8e0246e2e10982464bd677dd45a6f784c1e34564
Atulya Minz <atulminz.jsr@gmail.com>
2025-07-19 11:16:30 +0530

    Added boiler code

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract MerkleAirdrop {}
